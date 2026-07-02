235b15de7cb5b1d0edee2898d79b846ab48e5d68
People's Power & Peace Party (PPPP) <niazulhasanjewel@gmail.com>
2026-07-02 12:10:50 +0600

    Add Firebase configuration and app constants

---
+++ chat/config.js
@@ -0,0 +1,22 @@
+// PPPP Chat Firebase Configuration
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsvY3vQYLwSwbaS_YT7xwbHGSoREhj9wc",
+  authDomain: "pppp-chat.firebaseapp.com",
+  projectId: "pppp-chat",
+  storageBucket: "pppp-chat.firebasestorage.app",
+  messagingSenderId: "272715802614",
+  appId: "1:272715802614:web:e1881cd2a0aaf949d549d7",
+  measurementId: "G-NNEZLK09R6"
+};
+
+// Website Information
+export const APP_NAME = "People's Power & Peace Party";
+export const CHAT_VERSION = "1.0.0";
+
+// Firestore Collections
+export const COLLECTIONS = {
+  CHATS: "chats",
+  USERS: "users",
+  ADMINS: "admins"
+};
